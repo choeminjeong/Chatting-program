7fddf7e57bc727ae549e8174922958f9876a3708
choeminjeong <94516989+choeminjeong@users.noreply.github.com>
2021-12-01 14:43:59 +0900

    Add files via upload

--- client.c
+++ client.c
@@ -6,12 +6,17 @@
 #include <stdio.h>
 #include <string.h>
 #include <pthread.h>
+#include <ncurses.h>
 
 #define PORTNUM 9004
+#define HEIGHT 15
+#define WIDTH 55
 
 char sbuf[256], rbuf[256];
 int sd;
 int rlen;
+int r = 2, c = 2;
+int cnt = 0;
 
 void *recv_func(void *arg){
 	while(1){
@@ -19,24 +24,60 @@ void *recv_func(void *arg){
 		rlen = recv(sd, rbuf, sizeof(rbuf), 0);
 		if(strncmp(rbuf, "exit", 4) == 0)
 			exit(0);
-		if(rlen > 0)
-			printf("server : %s\n", rbuf);
+		if(rlen > 0){
+			//printf("server: %s", rbuf);	
+	
+			if(r >= 14){
+				r = 1;
+				clear();
+			}
+			move(r++, c);
+			printw("server: ");
+			printw("%s", rbuf);
+			//char *a = (char *)arg;
+			//mvwprintw((WINDOW *)arg, 1, 1, "hello");
+			refresh();
+		}
+		
 	}
 }
 
 void *send_func(void *arg){
 	while(1){
 		memset(sbuf, '\0', sizeof(sbuf));
-		printf("Enter message : %s\n", fgets(sbuf, sizeof(sbuf), stdin));
+		//printf("Enter message: %s\n", fgets(sbuf, sizeof(sbuf), stdin));
+		
+		move(30, 2);
+		//printf("client: ");
+		//scanf("%s", sbuf);
+		//printf("%s", sbuf);
+		printw("client: ");
+		scanw("%s", sbuf);
 		sbuf[strlen(sbuf)] = '\0';
-		if(send(sd, sbuf, strlen(sbuf)+1, 0) == -1){
+		
+		//printw("%s", sbuf);
+		//
+		
+		if(send(sd, sbuf, strlen(sbuf), 0) == -1){
 			perror("send");
 			exit(1);
 		}
+		
+		if(r >= 14){
+			r = 2;
+			clear();
+		}
+		move(r++, c);
+		//printw("client: ");
+		//printw("%s", sbuf);
+		refresh();
 		if(strncmp(sbuf, "exit", 4) == 0)
 			exit(0);
 	}
 }
+
+int startx = 0;
+int starty = 0;
 
 int main(){
 	char menu[256] = {0,};
@@ -69,10 +110,38 @@ int main(){
 	send(sd, menu, strlen(menu)+1, 0);
 
 	if(!strncmp(menu, "1", 1)){
-		printf("=====chatting start=====\n");
+		
+		initscr();
+		cbreak;
+		echo();
+		curs_set(0);
 
+		start_color();
+		init_pair(1, COLOR_GREEN, COLOR_BLACK);
+		bkgd(COLOR_PAIR(1));
+		
+		
+		WINDOW *win = newwin(HEIGHT, WIDTH, starty, startx);
+		refresh();
+		mvwprintw(win, 1, 1, "======chatting start==============\n");
+		//printf("=====chatting start=====\n");
+
+		box(win, 0, 0);
+		scrollok(win, TRUE);
+		wrefresh(win);
+
+		//WINDOW *subwin = newwin(HEIGHT, WIDTH, starty, startx);
+		//refresh();
+
+		//box(subwin, 30, 30);
+		//wrefresh(subwin);
+
+
+		//void *arg = win;
 		pthread_create(&pth1, NULL, send_func, NULL);
+		//wrefresh(win);
 		pthread_create(&pth2, NULL, recv_func, NULL);
+		//wrefresh(win);
 
 		pthread_join(pth1, NULL);
 		pthread_join(pth2, NULL);
@@ -80,6 +149,37 @@ int main(){
 	if(!strncmp(menu, "2", 1)){
 		printf("=====game 1=====\n");
 
+		while(1){
+			memset(sbuf, '\0', sizeof(sbuf));
+			printf("rock, sissor, paper: ");
+			scanf("%s", sbuf);
+			
+			if(send(sd, sbuf, strlen(sbuf)+1, 0) == -1){
+				perror("send");
+				exit(1);
+			}
+			
+			memset(rbuf, '\0', sizeof(rbuf));
+			rlen = recv(sd, rbuf, sizeof(rbuf), 0);
+			printf("SERVER: %s\n", rbuf);
+
+			if((!(strcmp(sbuf, "rock")) && !(strcmp(rbuf, "sissor"))) || ((!strcmp(sbuf, "sissor")) && !(strcmp(rbuf, "paper"))) || ((!strcmp(sbuf, "paper")) && !(strcmp(rbuf, "rock")))){
+			 	printf("=====CLIENT WIN!=====\n");
+				sleep(1);
+				exit(1);
+			}else if((!(strcmp(sbuf, "rock")) && !(strcmp(rbuf, "paper"))) || ((!strcmp(sbuf, "sissor")) && !(strcmp(rbuf, "rock"))) || ((!strcmp(sbuf, "paper")) && !(strcmp(rbuf, "sissor")))){
+			 	printf("=====CLIENT LOSE!=====\n");
+				sleep(1);
+				exit(1);
+			}else if((!(strcmp(sbuf, "rock")) && !(strcmp(rbuf, "rock"))) || ((!strcmp(sbuf, "sissor")) && !(strcmp(rbuf, "sissor"))) || ((!strcmp(sbuf, "paper")) && !(strcmp(rbuf, "paper")))){
+
+				printf("=====EVEN: PLAY AGAIN!=====\n");
+				sleep(1);
+			}else{
+				printf("=====THAT IS NOT CORRECT RSP!=====\n");
+				sleep(1);
+			}
+		}
 		sleep(1);
 		close(sd);
 	}
@@ -89,5 +189,7 @@ int main(){
 		sleep(1);
 		close(sd);
 	}
+	endwin();
 	close(sd);
 }
+
