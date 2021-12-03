1f59b075d695bea1ce464447de4e8a92e5f6df94
Seunghui Lee <52689963+dltmdgml@users.noreply.github.com>
2021-12-04 01:35:54 +0900

    Update client.c

--- client.c
+++ client.c
@@ -136,44 +136,43 @@ int main()
 		return 0;
 	} //end choice 2
 	if(choice == 3){ //start choice 3 baskin robbins
-		strcpy(menu, "3");
+			strcpy(menu, "3");
 		send(sd, menu, strlen(menu)+1, 0);
 		initscr();
+		curs_set(TRUE);
+		echo();
+
         start_color();
 		init_pair(1,COLOR_RED,COLOR_YELLOW);
 		init_pair(2,COLOR_BLUE,COLOR_BLACK);
 		init_pair(4,COLOR_YELLOW,COLOR_BLACK);
 		init_pair(5,COLOR_WHITE,COLOR_BLACK);
+
         int player=0;int computer=0;
         int num=0;
+
         srand((unsigned)time(NULL));
+
+		clear();
 		attron(COLOR_PAIR(1));
-        printw("------------baskinrobbins31 game-----------\n");
+        mvprintw(0,0,"------------baskinrobbins31 game-----------\n");
 		attroff(COLOR_PAIR(1));
-		refresh();
-		getch();
-		endwin();
 		init_pair(3,COLOR_GREEN,COLOR_BLACK);
 		attron(COLOR_PAIR(3));
-        printw("GAME START");
+        mvprintw(1,0,"GAME START");
 		attroff(COLOR_PAIR(3));
-		refresh();
 		attron(COLOR_PAIR(2));
-		initscr();
         printw("**Enter the number from 1 to 3**\n");
-		initscr();
 		attroff(COLOR_PAIR(2));
-		initscr();
-		refresh();
+		int j = 2;
         while(1){
+			int xx = 50;
 			refresh();
-
-			init_pair(5,COLOR_WHITE,COLOR_BLACK);
 			attron(COLOR_PAIR(5));
-	        printw("\n[player] how many numbers will you enter? : ");
+	        move(j, 0);
+			printw("[player] how many numbers will you enter? : ");
 			attroff(COLOR_PAIR(5));
-			refresh();
-	        scanf("%d",&player);
+	        scanw("%d",&player);
 	        if(player>3 || player<=0){
 	            printf("Only enter numbers from 1 to 3\n\n");
 	            continue;
@@ -181,27 +180,42 @@ int main()
 	        for(int i=1;i<=player;i++){
 	            num++;
 	            if(num<=31){
-	                printf("%d",num);
-	            }
+					move(j, xx+=4);
+	                printw("%d",num);
+					refresh();
+			    }
 	        }
-	        if(num>=31){
-	            printf("\n*** computer win! ***");
-	            break;
+	        if(num>31){
+				move(32, 0);
+	            printw("*** computer win! ***");
+	            refresh();
+				break;
 	        }
 	        computer = (rand()%3+1);
-	        printf("\n[computer]: %d\n",computer);
+			move(j+1, 0);
+			printw("[computer]: %d\n",computer);
+			refresh();
 
+			xx = 50;
 	        for(int i=0;i<computer;i++){
 	            num++;
 	            if(num<=31){
-	                printf("%d ",num);
+	                move(j+1, xx+=4);
+					printw("%d",num);
+					refresh();
 	            }
         	}
-	        if(num>=31){
-	            printf("\n*** player win! ***\n");
+	        if(num>31){
+	            move(32,0);
+				printw("*** player win! ***\n");
+				refresh();
 	            break;
         	}
+			j+=2;
 		}
+		sleep(10);
+		endwin();
+		close(sd);
 		return 0;
 	}
 	if(choice == 4){
